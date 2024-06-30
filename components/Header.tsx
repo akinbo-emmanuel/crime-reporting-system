ce4f8e99c50e6da85016f25a6adbe40221aaf5e8
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-06-30 20:36:54 +0100

    Updated Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import logo from "../assets/logo-cropped.png";
+import { Menu } from "lucide-react";
 
 const Header = () => {
   return (
     <header className="fixed w-full py-3 px-1.5 md:px-10 lg:px-16 flex items-center justify-between bg-white">
-      <Image
-        src="https://www.npf.gov.ng/images/logo.png"
-        alt="NPF logo"
-        width={300}
-        height={100}
-      />
+      <div className="flex items-center gap-2">
+        <Image src={logo} alt="NPF logo" width={60} height={60} />
+
+        <div className="flex flex-col font-extrabold text-lg text-[#022444]">
+          <p>BADAGRY LGA</p>
+          <p>CRIME REPORTING SYSTEM</p>
+        </div>
+      </div>
+
+      <Menu className="md:hidden" size={35}/>
 
       <nav className="hidden md:block">
         <ul className="flex items-center gap-8">
           <li>
-            <Link href="/" className="hover:font-semibold hover:text-[#022444]">Home</Link>
+            <Link href="/" className="hover:font-semibold hover:text-[#022444]">
+              Home
+            </Link>
           </li>
           <li>Admin</li>
           <li>
